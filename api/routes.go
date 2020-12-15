3d28fe96e517b44cc513a5f47217d8654c57dc35
ruslanBik4 <bik4ruslan@gmail.com>
2020-12-15 20:30:19 +0200

    [MOD] action

--- api/routes.go
+++ api/routes.go
@@ -6,8 +6,29 @@ package api
 
 import (
 	"github.com/ruslanBik4/httpgo/apis"
+	"github.com/valyala/fasthttp"
 )
 
 var (
-	ApiRoutes = apis.NewMapRoutes()
+	ApiRoutes   = apis.NewMapRoutes()
+	AdminRoutes = apis.ApiRoutes{
+		"/api/": {
+			Fnc:  HandleApiRedirect,
+			Desc: "show search results according range of characteristics",
+			// DTO:    &DTOSearch{},
+			Method: apis.POST,
+			// Resp:   search.RespGroups(),
+		},
+	}
+	SearchRoutes = apis.ApiRoutes{}
 )
+
+func HandleApiRedirect(ctx *fasthttp.RequestCtx) (interface{}, error) {
+	ctx.Redirect("http://back2.uppeople.co"+string(ctx.URI().Path()), fasthttp.StatusMovedPermanently)
+	return nil, nil
+}
+
+func init() {
+	ApiRoutes.AddRoutes(AdminRoutes)
+	ApiRoutes.AddRoutes(SearchRoutes)
+}
