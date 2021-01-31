b6f047c3c00ff9ae8e4f0626e4660aa4e725f387
ruslanBik4 <bik4ruslan@gmail.com>
2021-01-31 12:15:55 +0200

    [MOD] send candidate - companies

--- api/list.go
+++ api/list.go
@@ -78,7 +78,7 @@ FROM vacancies JOIN companies on (vacancies.company_id=companies.id)
 	JOIN vacancies_to_candidates vc on (vacancies.id = vc.vacancy_id)
 	JOIN platforms ON (vacancies.platform_id = platforms.id)
 	JOIN status_for_vacs sv on vc.status = sv.id
-	WHERE vacancies_to_candidates.candidate_id=$1`, ref.ViewCandidate.Id)
+	WHERE vc.candidate_id=$1`, ref.ViewCandidate.Id)
 	if err != nil {
 		logs.ErrorLog(err, "")
 	}
