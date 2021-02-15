9d435ea9526471bbbea9754335914378b111ded7
ruslanBik4 <bik4ruslan@gmail.com>
2021-02-15 22:40:26 +0200

    [MOD] edit company comments

--- api/vacancy.go
+++ api/vacancy.go
@@ -105,7 +105,7 @@ func HandleViewVacancy(ctx *fasthttp.RequestCtx) (interface{}, error) {
 		`select *, 
 			(select p.nazva from platforms p where v.platform_id=p.id) as platform,
 			(select s.nazva from seniorities s where v.seniority_id=s.id) as seniority,
-			(select c.name from company c where v.company_id=c.id) as company,
+			(select c.name from companies c where v.company_id=c.id) as company,
 			(select s.name from location_for_vacancies s where v.location_id=s.id) as location
 			from vacancies v
 			where id = $1
