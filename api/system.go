083f2117287f8a8d10f4efb93fed1f8a64a17706
ruslanBik4 <bik4ruslan@gmail.com>
2021-05-14 10:58:58 +0300

    [ADD] traces

--- api/system.go
+++ api/system.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"os/exec"
 	"runtime/trace"
-	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/ruslanBik4/dbEngine/dbEngine"
@@ -18,19 +17,23 @@ import (
 
 func HandleTrace(ctx *fasthttp.RequestCtx) (interface{}, error) {
 	trace.Stop()
-	cmd := exec.CommandContext(ctx, `sudo`, `go`, `tool`, `trace`, "dev.out")
+
+	const port = ":6060"
+	cmd := exec.CommandContext(ctx, `sudo`, `go`, `tool`, `trace`, "-http="+port, "dev.out")
 	cmd.Stdout = ctx.Response.BodyWriter()
 	cmd.Stderr = ctx.Response.BodyWriter()
-	err := cmd.Run()
-	s := string(ctx.Response.Body())
+	err := cmd.Start()
 	if err != nil {
+		s := string(ctx.Response.Body())
+
 		logs.DebugLog(s)
 
 		return s, errors.Wrap(err, cmd.String()+s)
 	}
 
-	parts := strings.Split(s, ":")
-	url := fmt.Sprintf("%s://%s%s/", ctx.URI().Scheme(), string(ctx.Host()), parts[len(parts)-1])
+	// parts := strings.Split(s, ":")
+	url := fmt.Sprintf("%s://%s%s/", ctx.URI().Scheme(), string(ctx.Host()), port)
+	// parts[len(parts)-1])
 	logs.DebugLog("redirect:", url)
 	ctx.Redirect(url, fasthttp.StatusMovedPermanently)
 
