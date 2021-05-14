6c6d1ff03c8fcfc4bf69a889a4ac2254e64bc56f
ruslanBik4 <bik4ruslan@gmail.com>
2021-05-14 10:41:37 +0300

    [ADD] traces

--- api/system.go
+++ api/system.go
@@ -5,7 +5,10 @@
 package api
 
 import (
+	"fmt"
 	"os/exec"
+	"runtime/trace"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/ruslanBik4/dbEngine/dbEngine"
@@ -14,16 +17,22 @@ import (
 )
 
 func HandleTrace(ctx *fasthttp.RequestCtx) (interface{}, error) {
+	trace.Stop()
 	cmd := exec.CommandContext(ctx, `sudo`, `go`, `tool`, `trace`, "dev.out")
 	cmd.Stdout = ctx.Response.BodyWriter()
 	cmd.Stderr = ctx.Response.BodyWriter()
 	err := cmd.Run()
+	s := string(ctx.Response.Body())
 	if err != nil {
-		s := string(ctx.Response.Body())
 		logs.DebugLog(s)
 
 		return s, errors.Wrap(err, cmd.String()+s)
 	}
+
+	parts := strings.Split(s, ":")
+	url := fmt.Sprintf("%s://%s%s/", ctx.URI().Scheme(), string(ctx.Host()), parts[len(parts)-1])
+	logs.DebugLog("redirect:", url)
+	ctx.Redirect(url, fasthttp.StatusMovedPermanently)
 
 	return nil, nil
 }
